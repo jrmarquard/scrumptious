a91962855f8b6ce3c9c2711e6dd3775fc5c27781
Joshua Morton <joshanator1995@gmail.com>
2016-10-19 10:54:44 +1100

    adding missing firebase imports

--- js/components/NavSite.js
+++ js/components/NavSite.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Nav, Navbar, NavDropdown, MenuItem, NavItem, FormGroup } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import firebase from 'firebase';
 
 export default class NNav extends React.Component {
     constructor() {
